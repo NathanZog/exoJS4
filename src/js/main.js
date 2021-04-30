d22529f94d961a9a3a40be264bb2e705b3f2d5a5
NathanZog <81653297+NathanZog@users.noreply.github.com>
2021-04-30 15:02:50 +0200

    fin exo4js

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,14 @@
 //let reponse = prompt("Comment t'appelles-tu ?");
-//alert("Bonjour"+ " "+reponse);
+//alert("Bonjour "+reponse);
 
 //let reponse = prompt("Quel age as-tu ?");
 //alert("Tu as " + reponse +" ans");
 
-//let reponse = prompt("Quel est votre nom complet ?");
-//alert("Vous vous appelez"+ " "+ reponse);
+//let reponse = prompt("Quel est votre nom ?")
+//let reponse2 =prompt("Quel est votre prénom ?")
+//alert("Bonjour "+reponse+ " "+reponse2)
 
 let reponse = prompt("Quel est votre nom ?")
 let reponse2 =prompt("Quel est votre prénom ?")
-alert("Vous vous appelez "+ (reponse.toUpperCase())+ " "+reponse2)
+let nomcomplet = reponse +" "+ reponse2
+alert("Bonjour "+ nomcomplet)
