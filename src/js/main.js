4311ce351971e8f1547930bbc4a04a8884a9aa19
NathanZog <81653297+NathanZog@users.noreply.github.com>
2021-04-30 14:25:31 +0200

    exojs4

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+//let reponse = prompt("Comment t'appelles-tu ?");
+//alert("Bonjour"+ " "+reponse);
+
+//let reponse = prompt("Quel age as-tu ?");
+//alert("Tu as " + reponse +" ans");
+
+//let reponse = prompt("Quel est votre nom complet ?");
+//alert("Vous vous appelez"+ " "+ reponse);
+
+let reponse = prompt("Quel est votre nom ?")
+let reponse2 =prompt("Quel est votre prénom ?")
+alert("Vous vous appelez "+ (reponse.toUpperCase())+ " "+reponse2)
